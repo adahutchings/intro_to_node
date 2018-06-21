52d68fc3351c0c7e245cd4715e131ae806051d52
Ada Hutchings <adalynn.hutchings@gmail.com>
2018-06-21 11:39:25 -0700

    Checkpoint 5 Basic Node App answers and code

---
+++ node-greeting-app/main.js
@@ -0,0 +1,26 @@
+const greetings = require("./greeting.js");
+const thanks = require("./thanks.js");
+
+//"hello"
+greetings.greetingEnglish();
+
+//"konichiwa"
+greetings.greetingJapanese();
+
+//"hola"
+greetings.greetingSpanish();
+
+//"hallo"
+greetings.greetingIcelandic();
+
+//"Thank You"
+thanks.thanksEnglish();
+
+//"Arigato"
+thanks.thanksJapanese();
+
+//"Gracias"
+thanks.thanksSpanish();
+
+//"Takk"
+thanks.thanksIclandic();
---
+++ node-greeting-app/thanks.js
@@ -0,0 +1,15 @@
+exports.thanksEnglish = function(){
+  console.log("Thank You");
+}
+
+exports.thanksJapanese = function(){
+  console.log("Arigato");
+}
+
+exports.thanksSpanish = function(){
+  console.log("Gracias");
+}
+
+exports.thanksIclandic = function(){
+  console.log("Takk");
+}
